5902553a99181b24b1f1af881e595606284a5909
kvk95 <kvk95@yahoo.com>
2024-08-08 09:27:17 +0530

    webscrapping

--- 001-streamlit/s29-192-193-006-streamlit-QandA-from-document/main.py
+++ 001-streamlit/s29-192-193-006-streamlit-QandA-from-document/main.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
 from utils.MyModels import BaseChatModel, LlmModel, init_llm
 from utils.MyUtils import clear_terminal, logger
 from PyPDF2 import PdfReader
@@ -7,12 +14,6 @@ import streamlit as st
 import sys
 from utils.MyVectorStore import chroma_from_documents
 from utils.MyEmbeddingFunction import SentenceEmbeddingFunction
-import os
-import sys
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
-sys.path.insert(0, parent_dir_path)
 
 ## Logging ##
 # clear_terminal()
@@ -44,10 +45,7 @@ def generate_response(file, openai_api_key, query):
     for page in reader.pages:
         formatted_document.append(page.extract_text())
     # split file
-    text_splitter = CharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=0
-    )
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.create_documents(formatted_document)
     # create embeddings
     embeddings = SentenceEmbeddingFunction()
@@ -60,53 +58,34 @@ def generate_response(file, openai_api_key, query):
 
     # create retrieval chain
     retrieval_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=store.as_retriever()
+        llm=llm, chain_type="stuff", retriever=store.as_retriever()
     )
     # run chain with query
     return retrieval_chain.run(query)
 
 
-st.set_page_config(
-    page_title="Q&A from a long PDF Document"
-)
+st.set_page_config(page_title="Q&A from a long PDF Document")
 st.title("Q&A from a long PDF Document")
 
-uploaded_file = st.file_uploader(
-    "Upload a .pdf document",
-    type="pdf"
-)
+uploaded_file = st.file_uploader("Upload a .pdf document", type="pdf")
 
 query_text = st.text_input(
     "Enter your question:",
     placeholder="Write your question here",
-    disabled=not uploaded_file
+    disabled=not uploaded_file,
 )
 
 result = []
-with st.form(
-    "myform",
-    clear_on_submit=True
-):
+with st.form("myform", clear_on_submit=True):
     openai_api_key = st.text_input(
-        "OpenAI API Key:",
-        type="password",
-        disabled=not (uploaded_file and query_text)
+        "OpenAI API Key:", type="password", disabled=not (uploaded_file and query_text)
     )
     submitted = st.form_submit_button(
-        "Submit",
-        disabled=not (uploaded_file and query_text)
+        "Submit", disabled=not (uploaded_file and query_text)
     )
     if submitted:  # and openai_api_key.startswith("sk-"):
-        with st.spinner(
-            "Wait, please. I am working on it..."
-        ):
-            response = generate_response(
-                uploaded_file,
-                openai_api_key,
-                query_text
-            )
+        with st.spinner("Wait, please. I am working on it..."):
+            response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
             del openai_api_key
 
